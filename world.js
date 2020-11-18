b75da68aeb846ecf787b2468f353d72d6f05a795
ksterling <ksterling542@gmail.com>
2020-11-18 03:43:32 -0500

    Finished but not styled

--- world.js
+++ world.js
@@ -14,6 +14,20 @@ window.onload = function(){
 
     });
 
+    var button2 = document.getElementById("city");
+    
+
+    button2.addEventListener("click",function(el){
+        el.preventDefault();
+        var url = "world.php";
+        var country = document.getElementById("country").value;
+        httpRequest.open('GET',url+"?country="+country+"&context=cities",false);
+        httpRequest.send();
+        httpRequest.onreadystatechange = getMsg();
+        
+
+    });
+
 function getMsg() {
     if (httpRequest.readyState == XMLHttpRequest.DONE)
         if (httpRequest.status == 200) {
--- world.php
+++ world.php
@@ -4,14 +4,16 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$country = $_GET['country'];
+$country = filter_input(INPUT_GET, 'country',FILTER_SANITIZE_STRING);
+$city = filter_input(INPUT_GET, 'context',FILTER_SANITIZE_STRING);
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if(empty($city)){
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<table border='1'>
+  echo "<table border='1'>
 
 <tr>
 <th>Name</th>
@@ -29,5 +31,30 @@ foreach ($results as $row){
   echo "</tr>";
 }
 echo "</table>";
+}else{
+
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT cities.district,cities.name,cities.population FROM cities JOIN countries ON countries.code=cities.country_code WHERE countries.name='$country'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  echo "<table border='1'>
+
+<tr>
+<th>Name</th>
+<th>District</th>
+<th>Population</th>
+</tr>";
+
+foreach ($results as $row){ 
+  echo "<tr>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['district'] . "</td>";
+  echo "<td>" . $row['population'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+}
+
+
 
 ?>
